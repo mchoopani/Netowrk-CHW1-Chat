2748837c674b0b786a23c9cdf56028ad7e0613cd
Mahmood Choopani <mahmood.choopani@divar.ir>
2023-12-18 19:45:48 +0330

    Add encoding to password.

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import abc
+import hashlib
 
 
 class PasswordIsWrong(Exception):
@@ -23,19 +24,24 @@ class FileSystemDatabase(DatabaseInterface):
         self.path = path
         open(self.path, "a+").close()
 
+    @classmethod
+    def get_encoded_password(cls, password):
+        return hashlib.sha256(password.encode('utf-8')).hexdigest()
+
     def get_user_if_exist(self, username: str, password: str):  # TODO: return user if needed
+        encoded_pass = self.get_encoded_password(password)
         with open(self.path, 'r') as f:
             for line in f.readlines():
                 if not line.startswith("user"):
                     continue
 
                 _, current_username, current_password = line.strip().split("###")
-                if current_username == username and current_password == password:
+                if current_username == username and current_password == encoded_pass:
                     return None, True
-                elif current_username == username and current_password != password:
+                elif current_username == username and current_password != encoded_pass:
                     raise PasswordIsWrong
             return None, False
 
     def save_user(self, username: str, password: str):
         with open(self.path, 'a') as f:
-            f.write(f"user###{username}###{password}\n")
+            f.write(f"user###{username}###{self.get_encoded_password(password)}\n")
